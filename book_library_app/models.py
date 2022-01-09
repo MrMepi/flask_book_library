6a684ec9553cab4351d1e41060e0ee67a4eb9732
MrMepi <mrmepix@gmail.com>
2022-01-09 17:14:37 +0100

    adding a table of books

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -18,6 +18,7 @@ class Author(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+    books = db.relationship('Book', back_populates='author', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<{self.__class__.__name__}>: {self.first_name} {self.last_name}'
@@ -92,6 +93,19 @@ class Author(db.Model):
             pagination['prev_page'] = url_for('authors.get_authors', page=page - 1, **params)
 
         return paginate_obj.items, pagination
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    isbn = db.Column(db.BigInteger, nullable=False, unique=True)
+    number_of_pager = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+    author = db.relationship('Author', back_populates='books')
+
+    def __repr__(self):
+        return f'{self.title} - {self.author.first_name} - {self.author.last_name}'
 
 class AuthorsSchema(Schema):
     id = fields.Integer(dump_only=True)
